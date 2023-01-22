f02aa720deb84b8c5fb62f0a5c81d8189a69ebfe
linux <pop@pop-os.localdomain>
2023-01-21 21:19:36 -0500

    menu

--- Scenes/Label.gd
+++ Scenes/Label.gd
@@ -2,4 +2,4 @@ extends Label
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("Q"):
-		get_tree().change_scene("res://Demo/Main.tscn")
+		get_tree().change_scene("res://Scenes/Menu.tscn")
